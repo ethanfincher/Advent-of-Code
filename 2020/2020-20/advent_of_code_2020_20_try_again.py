818d4ee76e7e8d7d9f2551abbc60eb3d547d6f24
Ethan Fincher <ethan.j.fincher@gmail.com>
2024-10-08 23:58:36 -0500

    2020-20 multi possibility bug

--- 2020/2020-20/advent_of_code_2020_20_try_again.py
+++ 2020/2020-20/advent_of_code_2020_20_try_again.py
@@ -17,23 +17,23 @@ def extract_edges(tile):
     left = tile[:, 0]
     right = tile[:, -1]
     return top, bottom, left, right
-
+already_used = []
 def matching_tile(edge_to_match, original_tile, side_to_match=None):
-    for tile in [tile for tile in tiles if not np.array_equal(tile, original_tile)]:
+    for index, tile in enumerate([tile for tile in tiles if not np.array_equal(tile, original_tile)]):
         edges = extract_edges(tile)
         if np.any(np.all(edges == edge_to_match, axis=1)) or np.any(np.all(edges == edge_to_match[::-1], axis=1)): 
             if side_to_match != None:
                 for _ in range(4):
                     if np.array_equal(extract_edges(tile)[side_to_match], edge_to_match): 
+                        del tiles[index]
                         return tile
                     tile = np.rot90(tile)
                 tile = tile[::-1, :]
                 for _ in range(4):
                     if np.array_equal(extract_edges(tile)[side_to_match], edge_to_match): 
+                        del tiles[index]
                         return tile
                     tile = np.rot90(tile)
-                exit()
-
             return tile
     return None
 
@@ -54,7 +54,8 @@ for r_index, row in enumerate(tile_grid):
         edges = extract_edges(current_tile)
         tile_grid[r_index][c_index+1] = matching_tile(edges[3], current_tile, 2)
 
-tile_grid = tile_grid[:, :, 1:-1, 1:-1]
+
+# tile_grid = tile_grid[:, :, 1:-1, 1:-1]
 original_shape = np.shape(tile_grid)
 combined_square_size = original_shape[0] * original_shape[2]
 combined_grid = np.empty((combined_square_size, combined_square_size), dtype=object)
@@ -64,6 +65,30 @@ for tr_index, tile_row in enumerate(tile_grid):
             for pc_index, pixel_col in enumerate(pixel_row):
                 combined_grid[tr_index*original_shape[2] + pr_index][tc_index*original_shape[2] + pc_index] = pixel_col
 
-combined_grid = np.rot90(combined_grid)
-combined_grid = np.rot90(combined_grid)
-combined_grid = np.rot90(combined_grid)
+multi_line_string = '\n'.join(' '.join(map(str, row)) for row in combined_grid)
+
+# Write to a text file
+with open('array_output.txt', 'w') as f:
+    f.write(multi_line_string)
+# sea_monsters = 0
+# counter = 0
+# while sea_monsters == 0:
+#     hash_count = 0
+#     total_spaces_checked = 0
+#     for index, value in np.ndenumerate(combined_grid):
+#         try:
+#             if value == "#":
+#                 hash_count += 1
+#                 if all(combined_grid[index[0]+location[0], index[1]+location[1]] == "#" for location in 
+#                         [(1,1), (1,0), (1,-1), (1,-6), (1,-7), (1,-12), (1,-13), (1,-18), (2,-2), (2,-5), (2,-8), (2,-11), (2,-14), (2,-17)]):
+#                     sea_monsters += 1
+#         except:
+#             continue
+#     if counter == 4: combined_grid = combined_grid[::-1]
+#     else: combined_grid = np.rot90(combined_grid)
+#     counter += 1
+# print(np.shape(tile_grid))
+# print(np.shape(combined_grid))
+# print(total_spaces_checked)
+# print(sea_monsters)
+# print(hash_count)
