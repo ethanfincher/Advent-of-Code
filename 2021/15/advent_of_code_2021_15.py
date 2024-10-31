6487bd7783c593a2527ecac73bd7431c8e5a6f3d
Ethan Fincher <ethan.j.fincher@gmail.com>
2024-10-31 02:09:38 -0500

    2021 15 2x (!)

--- 2021/15/advent_of_code_2021_15.py
+++ 2021/15/advent_of_code_2021_15.py
@@ -1,6 +1,22 @@
 import numpy as np
 cave = np.array([[int(num) for num in list(line)] for line in open("2021\\15\\input.txt").read().strip().split("\n")])
 
+# add dimensions to cave for part 2
+original_size = cave.shape
+# add horizontal dimensions
+for i in range(4):
+    new_array = cave[:, i*(original_size[1]):] + 1
+    # Change all 10s to 1s
+    new_array[new_array == 10] = 1
+    cave = np.hstack((cave, new_array))
+# add vertical dimensions
+for j in range(4):
+    new_array = cave[j*(original_size[0]):, :] + 1
+    # Change all 10s to 1s
+    new_array[new_array == 10] = 1
+    cave = np.vstack((cave, new_array))
+
+# stays the same for part 2, takes about 5 seconds
 max_size = cave.shape
 # Looks like dijkstra is the way to go
 shortest_paths = {index:10000000000 for index in np.ndindex(cave.shape)}
@@ -17,4 +33,3 @@ while queue:
             shortest_paths[neighbor] = shortest_paths[current_position] + cave[neighbor]
             queue[neighbor] = shortest_paths[neighbor]
 print(shortest_paths[(max_size[0]-1, max_size[1]-1)])
-    
