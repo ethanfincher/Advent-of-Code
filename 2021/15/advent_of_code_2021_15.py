85e9a3eb37ead56e60aae651905c59052876549b
Ethan Fincher <ethan.j.fincher@gmail.com>
2024-10-31 01:17:03 -0500

    part 1 faster and working (had to study)

--- 2021/15/advent_of_code_2021_15.py
+++ 2021/15/advent_of_code_2021_15.py
@@ -1,24 +1,20 @@
 import numpy as np
-import sys
-sys.setrecursionlimit(15000)
-print(sys.getrecursionlimit())
 cave = np.array([[int(num) for num in list(line)] for line in open("2021\\15\\input.txt").read().strip().split("\n")])
 
-exit_index = (cave.shape[0] - 1, cave.shape[1] - 1)
-lowest_path_sum = 10000000000000000
+max_size = cave.shape
+# Looks like dijkstra is the way to go
+shortest_paths = {index:10000000000 for index in np.ndindex(cave.shape)}
+shortest_paths[(0,0)] = 0
+really_big = 10000000000000000
+queue = {(0,0): shortest_paths[(0,0)]}
 
-def find_path(indexes_visited: list, total_value: int, current_index: tuple):
-    global lowest_path_sum
-    if total_value >= lowest_path_sum: return
-    if current_index != (0,0):
-        total_value += cave[current_index]
-    if current_index == exit_index:
-        if total_value < lowest_path_sum: 
-            lowest_path_sum = total_value
-    else:
-        for delta in [(-1,0), (1,0), (0,-1), (0,1)]:
-            neighbor = (current_index[0]-delta[0], current_index[1]-delta[1])
-            if (0 <= neighbor[0] <= exit_index[0] and 0 <= neighbor[1] <= exit_index[1]) and neighbor not in indexes_visited:
-                find_path(indexes_visited+[current_index], total_value, neighbor)
-find_path([], 0, (0,0))
-print(lowest_path_sum)
+while queue:
+    current_position = min(queue, key=queue.get)
+    del queue[current_position]
+    neighbors = [(current_position[0]-a, current_position[1]-b) for (a,b) in [(1,0),(-1,0),(0,1),(0,-1)] if 0 <= current_position[0]-a < max_size[0] and 0 <= current_position[1]-b < max_size[1]]
+    for neighbor in neighbors:
+        if shortest_paths[current_position] + cave[neighbor] < shortest_paths[neighbor]:
+            shortest_paths[neighbor] = shortest_paths[current_position] + cave[neighbor]
+            queue[neighbor] = shortest_paths[neighbor]
+print(shortest_paths[(max_size[0]-1, max_size[1]-1)])
+    
