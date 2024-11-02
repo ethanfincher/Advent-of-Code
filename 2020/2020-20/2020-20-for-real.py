0422e9a8686c3fb4221442e36eec18811a9fab10
Ethan Fincher <ethan.j.fincher@gmail.com>
2024-11-02 18:37:03 -0500

    2020 20 part 2 FINALLY

--- 2020/2020-20/2020-20-for-real.py
+++ 2020/2020-20/2020-20-for-real.py
@@ -1,94 +1,123 @@
-import numpy as np
+# my last failure at least showed that every side only matches with one other side, I just messed up how I was doing it at some point, and didnt comment my code so its impossible to go back to
 import re
+import numpy as np
 import math
-raw_tiles = open("2020\\2020-20\\input.txt").read().strip().split("\n\n")
-numbers = [int(re.search(r'Tile (\d*):', tile).group(1)) for tile in raw_tiles]
-tiles = np.array([[list(line) for line in tile.split("\n")[1:]] for tile in raw_tiles])
-sides = {}
+# Create dictionary of tile_number: 2d array of tile
+tiles_list = [tile.split("\n") for tile in open("2020\\2020-20\\input.txt").read().strip().split("\n\n")]
+tiles = {int(re.search(r"Tile (\d*):", rows[0]).group(1)): np.array([list(row) for row in rows[1:]]) for rows in tiles_list}
+possible_tiles = list(tiles.keys())
 
+# Find top left corner
 def get_edges(tile):
-    return {"top": tile[0, :], "bottom": tile[-1, :], "left": tile[:, 0], "right": tile[:, -1]}
+    # Top = 0, Right = 1, Bottom = 2, Left = 3
+    return np.array((tile[0, :], tile[:, -1], tile[-1, :], tile[:, 0]))
 
-for index, tile in enumerate(tiles):
-    sides[numbers[index]] = get_edges(tile)
+# 3d list of all edges
+all_edges = np.array([get_edges(tile) for tile in tiles.values()])
+# flattens into 2d array
+all_edges = all_edges.reshape(-1, all_edges.shape[-1])
+# add inversions
+all_edges = np.concatenate((all_edges, all_edges[:, ::-1]), axis=0)
+
+# values are the actual tiles, keys are numbers
+for number, tile in tiles.items():
+    current_edges = get_edges(tile)
+    # if top and left edges only show up in all edges once (just themselves, no matches), then that tile is top left
+    if all((np.sum(np.all(all_edges == edge, axis=1)) == 1 for edge in [current_edges[0], current_edges[3]])):
+        top_left = tile
+        top_left_number = number
+        break
+
+# Create empty grid
+# creates square of squares. outer square is sqrt the number of total tiles, inner squares are the same dimention as the top left tile
+tile_grid = np.empty((int(math.sqrt(len(tiles_list))), int(math.sqrt(len(tiles_list))), len(top_left), len(top_left[0])), dtype='<U1')
+# add top left to grid
+tile_grid[0,0] = top_left
+# once a tile has been added to the grid, remove it from possible tiles
+possible_tiles.remove(top_left_number)
+
+def find_matching_tile(starting_tile, match_right):
+    # if we're matching to the right, starting side is the right side, and we're matching the left side of a tile to it
+    if match_right:
+        starting_side = get_edges(starting_tile)[1]
+        side_to_match = 3
+    # if we're not matching the right, then wer're matching the bottom, so sude to match is the top
+    else: 
+        starting_side = get_edges(starting_tile)[2]
+        side_to_match = 0
     
-# this takes a couple seconds, but at this point I'm just gonna go for it
-matching_sides = {}
-# for each tile
-for tile_number, tile_sides in sides.items():
-    # find and label all matching sides
-    matches = {"top": 0, "bottom": 0, "left": 0, "right": 0}
-    # go through each side name and its corresponding side (list of chars)
-    for side_name, side in tile_sides.items():
-        # then go through all other tiles, 
-        for different_tile_number, different_tile_sides in sides.items():
-            if different_tile_number == tile_number: continue
-            # if any of its sides match, add it to matches
-            if any(np.array_equal(side, different_side) or np.array_equal(side[::-1], different_side) for different_side in different_tile_sides.values()):
-                matches[side_name] = different_tile_number
-    matching_sides[tile_number] = matches
-# you can get all 4 corners for part 1 from here, I'm going straight to part 2
-
-# match all the numbers up first (to make sure im not going crazy)
-top_left = {key: value for key, value in matching_sides.items() if not value['top'] and not value['left']}
-# starting with the top left, get the first row of numbers
-
-# make empty 2d number grid
-number_grid = np.zeros(((int(math.sqrt(len(tiles)))), (int(math.sqrt(len(tiles))))))
-number_grid_shape = number_grid.shape
-number_grid[0,0] = list(top_left.keys())[0]
-
-directions = ["top", "right", "bottom", "left"]
-def make_number_grid(side_to_match, current_tile_number, matching_position, direction):
-    print(f"current grid: \n{number_grid}")
-    print(f"moving {direction}")
-    # find which number is to the right of the current number
-    matching_tile_number = matching_sides[current_tile_number][side_to_match]
-    print(matching_sides[matching_tile_number])
-    # assign that index
-    number_grid[matching_position] = matching_tile_number
-    print(f"number {matching_tile_number} matched")
-    # find which side matches up
-    matching_side = next((key for key, value in matching_sides[matching_tile_number].items() if value == current_tile_number))
-    print(f"{matching_side} lines up")
-    # find the opposite side and repeat. stop when the opposite side doesnt exist. when it doesnt, move down one, then find a match to the left
-    # if going right and you hit the edge
-    if direction == "right" and matching_position[1] == number_grid_shape[1] - 1:
-        if matching_position[0] == number_grid_shape[0] - 1: return
-        next_direction = "down"
-        next_position = (matching_position[0] + 1, matching_position[1])
-        next_side_to_match = directions[(directions.index(matching_side) - 1) % 3]
-    # going left and hit the edge
-    elif direction == "left" and matching_position[1] == 0:
-        if matching_position[0] == number_grid_shape[0] - 1: return
-        next_direction = "down"
-        next_position = (matching_position[0] + 1, matching_position[1])
-        next_side_to_match = directions[(directions.index(matching_side) + 1) % 3]
-    # just moved down and are on right edge
-    elif direction == "down" and matching_position[1] == number_grid_shape[1] - 1:
-        next_direction = "left"
-        next_position = (matching_position[0], matching_position[1] - 1)
-        next_side_to_match = directions[(directions.index(matching_side) - 1) % 3]
-    # just moved down and hit the left edge
-    elif direction == "down" and matching_position[1] == 0:
-        next_direction = "right"
-        next_position = (matching_position[0], matching_position[1] + 1)
-        next_side_to_match = directions[(directions.index(matching_side) + 1) % 3]
-    # keep moving right or left
-    else:
-        next_direction = direction
-        next_position = (matching_position[0], matching_position[1] + (1 if direction == "right" else -1))
-        next_side_to_match = directions[(directions.index(matching_side) + 2) % 4]
-    print(f"next tile needs to match {next_side_to_match} side")
-    print()
-    make_number_grid(next_side_to_match, matching_tile_number, next_position, next_direction)
+    # find the matching tile
+    for possible_tile_number in possible_tiles:
+        possible_tile = tiles[possible_tile_number]
+        if np.any(np.all((np.append(get_edges(possible_tile), get_edges(possible_tile)[:, ::-1], axis=0)) == starting_side, axis=1)):
+            matching_tile = possible_tile
+            # remove tile once its been found (since it will be added later and we have the number now)
+            possible_tiles.remove(possible_tile_number)
+            break
+    # rotate the tile until the correct side matches the starting tile
+    flips = 0
+    while not np.array_equal(get_edges(matching_tile)[side_to_match], starting_side):
+        # if this doesn't work, inverse and try again
+        if flips == 4:
+            matching_tile = matching_tile[:, ::-1]
+            flips = 0
+        else:
+            matching_tile = np.rot90(matching_tile)
+            flips += 1
+    return matching_tile
 
 
 
-make_number_grid("right", list(top_left.keys())[0], (0,1), "right")
+for row in range(len(tile_grid)):
+    # match, rotate, and add all tiles to the right of it
+    for col in range(len(tile_grid[0])):
+        # last col will have already been added
+        if col != len(tile_grid[0]) - 1:
+            tile_grid[row, col+1] = find_matching_tile(tile_grid[row, col], True)
+    # then find the tile below the current tile, and repeat the steps until all tiles are added. dont go past last row
+    if row != len(tile_grid) - 1:
+        tile_grid[row+1, 0] = find_matching_tile(tile_grid[row, 0], False)
 
+# removes borders of tiles
+tile_grid = tile_grid[:, :, 1:-1, 1:-1]
+# This turns the 4d array into a 2d array
+original_shape = np.shape(tile_grid)
+combined_square_size = original_shape[0] * original_shape[2]
+combined_grid = np.empty((combined_square_size, combined_square_size), dtype=object)
+for tr_index, tile_row in enumerate(tile_grid):
+    for tc_index, tile_col in enumerate(tile_row):
+        for pr_index, pixel_row in enumerate(tile_col):
+            for pc_index, pixel_col in enumerate(pixel_row):
+                combined_grid[tr_index*original_shape[2] + pr_index][tc_index*original_shape[2] + pc_index] = pixel_col
 
-# tiles_shape = tiles.shape
-# tile_grid = np.empty((int(math.sqrt(tiles_shape[0])), int(math.sqrt(tiles_shape[0])), tiles_shape[1], tiles_shape[2]), dtype=object)
-# tile_grid[0,0] = tiles[numbers.index(list(top_left.keys())[0])]
+# # Write to a text file
+# multi_line_string = '\n'.join(''.join(map(str, row)) for row in combined_grid)
+# with open('array_output.txt', 'w') as f:
+#     f.write(multi_line_string)
 
+sea_monsters = 0
+counter = 0
+# this flips the picture if no sea monsters were found
+while sea_monsters == 0:
+    hash_count = 0
+    total_spaces_checked = 0
+    # this checks each hash, its inefficient, but works
+    for index, value in np.ndenumerate(combined_grid):
+        # try catch incase if all statement goes out of bounds
+        try:
+            if value == "#":
+                # add all hashes, then subtract sea monsters at the end
+                hash_count += 1
+                # check for sea monsters
+                if all(combined_grid[index[0]+location[0], index[1]+location[1]] == "#" for location in 
+                        [(1,1), (1,0), (1,-1), (1,-6), (1,-7), (1,-12), (1,-13), (1,-18), (2,-2), (2,-5), (2,-8), (2,-11), (2,-14), (2,-17)]):
+                    sea_monsters += 1
+        except:
+            continue
+    # if the picture has been flipped 4 times, invert it
+    if counter == 4: combined_grid = combined_grid[::-1]
+    # otherwise, flip it
+    else: combined_grid = np.rot90(combined_grid)
+    counter += 1
+
+print(hash_count - (sea_monsters*15))
