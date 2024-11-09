4feb505b429701364fce831569041fd2fc853db0
Ethan Fincher <ethan.j.fincher@gmail.com>
2024-11-08 23:38:34 -0600

    2015 3 2x

--- 2015/3/advent_of_code_2015_3.py
+++ 2015/3/advent_of_code_2015_3.py
@@ -1,7 +1,9 @@
 directions = open("2015/3/input.txt").read().strip()
+# all positions start at 0,0
 houses = [(0,0)]
-x,y = 0,0
-for direction in directions:
+santa_x, robo_x, santa_y, robo_y = 0,0,0,0
+
+def move_houses(x, y, direction):
     if direction == "<":
         x -= 1
     elif direction == ">":
@@ -11,4 +13,11 @@ for direction in directions:
     elif direction == "v":
         y -= 1
     houses.append((x,y))
+    return x,y
+
+for index, direction in enumerate(directions):
+    if index % 2 == 0:
+        santa_x, santa_y = move_houses(santa_x, santa_y, direction)
+    else:
+        robo_x, robo_y = move_houses(robo_x, robo_y, direction)
 print(len(set(houses)))
