aad00c71b9abd3e57954febe3b6dac000e1034f3
Ethan Fincher <ethan.j.fincher@gmail.com>
2024-11-16 19:35:44 -0600

    2015 9 2x

--- 2015/9/advent_of_code_2015_9.py
+++ 2015/9/advent_of_code_2015_9.py
@@ -9,8 +9,19 @@ for line in lines:
     city_routes[f"{line_regex.group(1)},{line_regex.group(2)}"] = int(line_regex.group(3))
 all_cities = list(set(all_cities))
 
-# print(max(city_routes, key=city_routes.get))
-min_distance = 100000000000000
+max_distance = 0
+
+def travel_cities(current_city, remaining_cities, total_distance):
+    if not remaining_cities:
+        global max_distance
+        # for part 2, just change min to max and change less than to greater than
+        if total_distance > max_distance:
+            max_distance = total_distance
+    else:
+        for remaining_city in remaining_cities:
+            route_distance = next(iter([value for key, value in city_routes.items() if all(city in key for city in [current_city, remaining_city])]), None)
+            travel_cities(remaining_city, [city for city in remaining_cities if city != remaining_city], total_distance + route_distance)
+
 for starting_city in all_cities:
-    remaining_cities = [city for city in all_cities if city != starting_city]
-    
+    travel_cities(starting_city, [city for city in all_cities if city != starting_city], 0)
+print(max_distance)
