f650b6cbca984a380291372c966050728fde0984
Ethan Fincher <ethan.j.fincher@gmail.com>
2024-11-30 19:07:48 -0600

    brute force not working, moving on

--- 2015/19/advent_of_code_2015_19.py
+++ 2015/19/advent_of_code_2015_19.py
@@ -1,16 +1,11 @@
 import re
 from collections import defaultdict
+from functools import lru_cache
 
 
 input_parts = open("2015/19/input.txt").read().strip().split("\n\n")
 replacement_strings = input_parts[0].split("\n")
 starting_string = input_parts[1]
-replacements = {}
-for line in replacement_strings:
-    line_parts = line.split(" => ")
-    replacements[line_parts[1]] = line_parts[0]
-
-print(replacements)
     
 # Part 1
 # replacements = defaultdict(list)
@@ -31,3 +26,27 @@ print(replacements)
 
 # print(len(unique_molecules))
 
+# Part 2
+replacements = {}
+for line in replacement_strings:
+    line_parts = line.split(" => ")
+    replacements[line_parts[1]] = line_parts[0]
+
+@lru_cache()
+def simplify_molecule(current_string, steps):
+    min_steps = 10000000000
+    print(current_string)
+    if current_string == "e":
+        return steps
+    if current_string.count("e") != 0:
+        return None
+    else:
+        for index, char in enumerate(current_string):
+            for replacement in replacements:
+                if current_string[index: index+len(replacement)] == replacement:
+                    new_step_count = simplify_molecule(f"{current_string[:index]}{replacements[replacement]}{current_string[index+len(replacement):]}", steps+1)
+                    if new_step_count and new_step_count < min_steps:
+                        min_steps = new_step_count
+        return min_steps
+
+print(simplify_molecule("starting_string", 0))
