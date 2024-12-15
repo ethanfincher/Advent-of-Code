4e252b5142a233112fa69777bf659434d2d88119
Ethan Fincher <ethan.j.fincher@gmail.com>
2024-12-15 01:53:13 -0600

    2024 14 comments, cuz i committed late

--- 2024/14/advent_of_code_2024_14.py
+++ 2024/14/advent_of_code_2024_14.py
@@ -18,6 +18,11 @@ def calc_robot_positions(width, height, seconds):
 
 # print(calc_robot_positions(101, 103, 100))
 
+# for part 2, i started by creating txt files of every second. on 128, i noticed they were much more centered then in all the other pictures
+# I noticed the same thing on 229, so went to check for a pattern every 101 pcitures and sure enough it was consistent.
+# from there I just looked through every 101 and eventually found it
+# also, the hardest part of this was figuring out how to page through the txt files in vs code.
+# for posterity, open every file, then use control pgdn/pgup to quickly run through them all. figuring that out took wayyyyyy longer then it should have
 def find_christmas_tree(width, height, pictures, start, increment):
     for picture in range(1, pictures+1):
         robot_positions = []
